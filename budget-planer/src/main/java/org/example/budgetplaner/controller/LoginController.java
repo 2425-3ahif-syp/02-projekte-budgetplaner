6cffa3422721c86e2ac50c8921841092a0caa682
Lena Graßauer <116261857+LenaGrassauer@users.noreply.github.com>
2025-04-06 17:08:25 +0200

    feat: profile picture

--- budget-planer/src/main/java/org/example/budgetplaner/controller/LoginController.java
+++ budget-planer/src/main/java/org/example/budgetplaner/controller/LoginController.java
@@ -1,14 +1,23 @@
 package org.example.budgetplaner.controller;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import static org.example.budgetplaner.controller.Menubar.createMenuBar;
 
+
 public class LoginController {
+
+    private static String gespeicherterName = "Max Mustermann";
+    private static String gespeicherterGeburtstag = "01.01.2001";
+    private static String gespeicherteEmail = "maxmustermann@gmail.com";
+    private static String gespeichertesPasswort = "geheim123";
 
     public static VBox createLoginView(Stage primaryStage) {
         VBox loginLayout = new VBox(10);
@@ -39,84 +48,96 @@ public class LoginController {
     public static Scene createAccountScene(Stage primaryStage, boolean editMode) {
         MenuBar menuBar = createMenuBar(primaryStage);
 
-        final String[] gespeicherterName = {"Max Mustermann"};
-        final String[] gespeicherterGeburtstag = {"01.01.2001"};
-        final String[] gespeicherteEmail = {"maxmustermann@gmail.com"};
-        final String[] gespeichertesPasswort = {"geheim123"};
-
-        GridPane grid = new GridPane();
-        grid.setPadding(new Insets(20));
-        grid.setVgap(10);
-        grid.setHgap(10);
+        GridPane formGrid = new GridPane();
+        formGrid.setPadding(new Insets(40));
+        formGrid.setVgap(15);
+        formGrid.setHgap(10);
+        ColumnConstraints col1 = new ColumnConstraints();
+        col1.setPercentWidth(40);
+        ColumnConstraints col2 = new ColumnConstraints();
+        col2.setPercentWidth(60);
+        formGrid.getColumnConstraints().addAll(col1, col2);
 
         Label nameLabel = new Label("Name:");
-        TextField nameField = new TextField(gespeicherterName[0]);
+        TextField nameField = new TextField(gespeicherterName);
         nameField.setEditable(editMode);
 
         Label geburtstagLabel = new Label("Geburtstag:");
-        TextField geburtstagField = new TextField(gespeicherterGeburtstag[0]);
+        TextField geburtstagField = new TextField(gespeicherterGeburtstag);
         geburtstagField.setEditable(editMode);
 
         Label emailLabel = new Label("E-Mail:");
-        TextField emailField = new TextField(gespeicherteEmail[0]);
+        TextField emailField = new TextField(gespeicherteEmail);
         emailField.setEditable(editMode);
 
         Label passwortLabel = new Label("Passwort:");
         PasswordField passwortField = new PasswordField();
-        passwortField.setText(gespeichertesPasswort[0]);
+        passwortField.setText(gespeichertesPasswort);
         passwortField.setEditable(editMode);
 
-        Button editButton = new Button("Bearbeiten");
-        editButton.setOnAction(e -> primaryStage.setScene(createAccountScene(primaryStage, true)));
-
-        if (editMode) {
-            Button saveButton = new Button("Speichern");
-            saveButton.setOnAction(e -> {
-                gespeicherterName[0] = nameField.getText();
-                gespeicherterGeburtstag[0] = (geburtstagField.getText());
-                gespeicherteEmail[0] = (emailField.getText());
-                gespeichertesPasswort[0] = (passwortField.getText());
-                primaryStage.setScene(createAccountScene(primaryStage, false));
-            });
-
-            grid.add(saveButton, 1, 5);
-        } else {
-            grid.add(editButton, 1, 5);
-        }
-
-        grid.add(nameLabel, 0, 0);
-        grid.add(nameField, 1, 0);
-        grid.add(geburtstagLabel, 0, 1);
-        grid.add(geburtstagField, 1, 1);
-        grid.add(emailLabel, 0, 2);
-        grid.add(emailField, 1, 2);
-        grid.add(passwortLabel, 0, 3);
-        grid.add(passwortField, 1, 3);
 
         ToggleGroup currencyGroup = new ToggleGroup();
-        RadioButton euroButton = new RadioButton("€");
+        RadioButton euroButton   = new RadioButton("€");
         RadioButton dollarButton = new RadioButton("$");
         euroButton.setToggleGroup(currencyGroup);
         dollarButton.setToggleGroup(currencyGroup);
         euroButton.setSelected(true);
 
-        if (editMode) {
-            euroButton.setDisable(false);
-            dollarButton.setDisable(false);
-        }
-        else {
-            euroButton.setDisable(true);
-            dollarButton.setDisable(true);
-        }
-
-
+        euroButton.setDisable(!editMode);
+        dollarButton.setDisable(!editMode);
         HBox currencyBox = new HBox(10, euroButton, dollarButton);
-        grid.add(currencyBox, 1, 4);
+
+
+        if (editMode) {
+            Button saveButton = new Button("Speichern");
+            saveButton.setOnAction(e -> {
+                gespeicherterName       = nameField.getText();
+                gespeicherterGeburtstag = geburtstagField.getText();
+                gespeicherteEmail       = emailField.getText();
+                gespeichertesPasswort   = passwortField.getText();
+                primaryStage.setScene(createAccountScene(primaryStage, false));
+            });
+            formGrid.add(saveButton, 1, 5);
+        } else {
+            Button editButton = new Button("Bearbeiten");
+            editButton.setOnAction(e -> primaryStage.setScene(createAccountScene(primaryStage, true)));
+            formGrid.add(editButton, 1, 5);
+        }
+
+
+        formGrid.add(nameLabel,       0, 0);
+        formGrid.add(nameField,       1, 0);
+        formGrid.add(geburtstagLabel, 0, 1);
+        formGrid.add(geburtstagField, 1, 1);
+        formGrid.add(emailLabel,      0, 2);
+        formGrid.add(emailField,      1, 2);
+        formGrid.add(passwortLabel,   0, 3);
+        formGrid.add(passwortField,   1, 3);
+        formGrid.add(currencyBox,     1, 4);
+
+
+        Image image = new Image("file:images/profile.png", true);
+        ImageView imageView = new ImageView(image);
+        imageView.setPreserveRatio(true);
+        imageView.setFitWidth(250);
+
+        VBox imageBox = new VBox(imageView);
+        imageBox.setAlignment(Pos.TOP_CENTER);
+        imageBox.setPadding(new Insets(40, 20, 20, 30));
+        imageBox.setMaxWidth(Double.MAX_VALUE);
+
+
+
+        HBox contentBox = new HBox(50, formGrid, imageBox);
+        contentBox.setPadding(new Insets(20));
+        HBox.setHgrow(formGrid, Priority.ALWAYS);
+
 
         BorderPane root = new BorderPane();
         root.setTop(menuBar);
-        root.setCenter(grid);
+        root.setCenter(contentBox);
 
-        return new Scene(root, 800, 600);
+        return new Scene(root, 1000, 600);
     }
+
 }
