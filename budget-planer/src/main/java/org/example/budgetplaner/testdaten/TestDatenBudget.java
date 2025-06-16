ba44546467a3146f90efcb8e74e94f7ea5726591
FeichtingerLeona <116262109+FeichtingerLeona@users.noreply.github.com>
2025-06-16 13:48:26 +0200

    fix: #BGP-6 Bekommen keinen error mehr

--- budget-planer/src/main/java/org/example/budgetplaner/testdaten/TestDatenBudget.java
+++ budget-planer/src/main/java/org/example/budgetplaner/testdaten/TestDatenBudget.java
@@ -1,11 +1,14 @@
 package org.example.budgetplaner.testdaten;
 
 import org.example.budgetplaner.databasepack.database.BudgetReposetory;
+import org.example.budgetplaner.databasepack.database.Database;
 
 import java.io.IOException;
 
 public class TestDatenBudget {
     public static void main(String[] args){
+        Database database = Database.getInstance();
+
         BudgetReposetory budgetReposetory = new BudgetReposetory();
         budgetReposetory.createNewBudgetPlan(2000.00f, 6, 2025, 1);
         budgetReposetory.createNewBudgetPlan(100.00f, 6, 2025, 2);
